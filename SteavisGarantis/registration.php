007b585c47aac79df6f99ac0451a21601138d837
savan13 <38579373+savan13@users.noreply.github.com>
2022-03-23 13:10:19 +0530

    Check

--- SteavisGarantis/registration.php
+++ SteavisGarantis/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Mageplaza_SteavisGarantis',
+	'Mageplaza_SteavisGarantis   ',
 	__DIR__
 );
